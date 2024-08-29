daefb78f4a1b0a645f09961957297f61f42dbf72
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-29 15:36:13 -0600

    Up

--- models/purchase_order_report.py
+++ models/purchase_order_report.py
@@ -5,10 +5,10 @@ class PurchaseOrderReport(models.Model):
 
     def action_print_purchase_order_custom(self):
         # Obtenemos el objeto de la empresa actual
-        company = self.env.user.company_id
+        company_data = self.env.user.company_id
         
-        # Añadimos 'company' al contexto antes de generar el reporte
+        # Añadimos 'company_data' al contexto antes de generar el reporte
         return self.env.ref('purchase_custom_report.action_report_purchase_order_custom').with_context(
             disable_report_customization=True,
-            company=company  # Pasamos el objeto 'company' al contexto
+            company_data=company_data  # Pasamos el objeto 'company' al contexto con un nombre diferente
         ).report_action(self)
