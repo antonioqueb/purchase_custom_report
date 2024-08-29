0236247b8a1ab120039df04118799541513be81a
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-29 15:29:01 -0600

    Up

--- models/purchase_order_report.py
+++ models/purchase_order_report.py
@@ -4,5 +4,11 @@ class PurchaseOrderReport(models.Model):
     _inherit = 'purchase.order'
 
     def action_print_purchase_order_custom(self):
-        # Llamamos directamente al reporte sin abrir la interfaz de branding
-        return self.env.ref('purchase_custom_report.action_report_purchase_order_custom').with_context(disable_report_customization=True).report_action(self)
+        # Obtenemos el objeto de la empresa actual
+        company = self.env.user.company_id
+        
+        # Añadimos 'company' al contexto antes de generar el reporte
+        return self.env.ref('purchase_custom_report.action_report_purchase_order_custom').with_context(
+            disable_report_customization=True,
+            company=company  # Pasamos el objeto 'company' al contexto
+        ).report_action(self)
