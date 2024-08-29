3ef1c375be37244e82449a7b69dd2789eb531fb3
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-29 15:40:18 -0600

    Up

--- models/purchase_order_report.py
+++ models/purchase_order_report.py
@@ -1,14 +1,18 @@
-from odoo import models
+from odoo import models, api
 
 class PurchaseOrderReport(models.Model):
     _inherit = 'purchase.order'
 
     def action_print_purchase_order_custom(self):
-        # Obtenemos el objeto de la empresa actual
+        return self.env.ref('purchase_custom_report.action_report_purchase_order_custom').report_action(self)
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['purchase.order'].browse(docids)
         company_data = self.env.user.company_id
-        
-        # Añadimos 'company_data' al contexto antes de generar el reporte
-        return self.env.ref('purchase_custom_report.action_report_purchase_order_custom').with_context(
-            disable_report_customization=True,
-            company_data=company_data  # Pasamos el objeto 'company' al contexto con un nombre diferente
-        ).report_action(self)
+        return {
+            'doc_ids': docids,
+            'doc_model': 'purchase.order',
+            'docs': docs,
+            'company_data': company_data,  # Pasamos el objeto 'company' al contexto con un nombre diferente
+        }
